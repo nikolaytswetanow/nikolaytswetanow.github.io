db6721dff20e882075bb999df2265b6e1b6ab3d7
nikolaytswetanow <nikolaytswetanow@gmail.com>
2023-03-09 23:22:28 +0200

    contactform design

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,6 +60,14 @@
     ScrollReveal({ duration: 1200, distance: '350px' }).reveal('.bot', { delay: 200, origin: 'bottom' });
     ScrollReveal({ duration: 1200, distance: '50px' }).reveal('.top', { delay: 200, origin: 'top' });
 
+    //Reset scroll top
 
+    if (history.scrollRestoration) {
+        history.scrollRestoration = 'manual';
+    } else {
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
+    }
 
 }(jQuery))
